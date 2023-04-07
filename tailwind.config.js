2d25b3e6cb198a8d51cd776cec3630ec9a58bd4d
justinT-compsci <thomasjn19@gmail.com>
2023-04-07 09:05:15 -0400

    add favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports =  {
   theme: {
     extend: {
       colors: {
-        'body': '#000012',
+        'body': '#000011',
       },
       fontFamily: {
         syne: "'Syne', 'sans-serif'",
